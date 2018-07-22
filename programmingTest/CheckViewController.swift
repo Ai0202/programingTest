b395bf93b6114a88d9d01bc6524ee04d5ed186c5
Atsushi <>
2018-07-22 21:48:42 +0800

    チェック画面追加、アイコン変更

---
+++ programmingTest/CheckViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CheckViewController.swift
+//  programmingTest
+//
+//  Created by Atsushi on 2018/07/22.
+//  Copyright © 2018年 Atsushi. All rights reserved.
+//
+
+import UIKit
+
+class CheckViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
